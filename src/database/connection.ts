768566b7827f56fb43f42eaa48297b8c48da41cf
jonathas araujo <jonathas.santos@grupoicts.com.br>
2020-08-07 22:20:07 -0400

    fix: add dotenv

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,11 +1,12 @@
 import knex from 'knex';
+require('dotenv').config();
 
 const connection = knex({
   client: 'mysql',
   connection: {
     host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
   },
 });
